bf54daedbc99fe3cf972f95cbca8950026f1d4cd
voytekio <voytek@voytek.io>
2018-11-23 21:43:31 +0000

    tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,9 @@
 import os
 import pytest
 
+print('------------- vars as seen by pytest -----------')
+print(os.environ)
+print('------------- end of vars as seen by pytest -----------')
 on_jenkins = 1 if os.environ.get('HUDSON_URL') else 0
 
 print('on_jenkins: {}'.format(on_jenkins))
