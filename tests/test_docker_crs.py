4e17233e7655bf0aeb1a76e3c6e969e55dfdafce
voytekio <voytek@voytek.io>
2018-11-24 01:44:03 +0000

    docker..

--- tests/test_docker_crs.py
+++ tests/test_docker_crs.py
@@ -1,6 +1,6 @@
 def test_with_container(docker_cr):
     #assert docker_cr.check_output('pwd') == '/'
-    res1 = docker.cr.run('ls -l /')
+    res1 = docker_cr.run('ls -l /')
     print(res1)
     assert 1==1
 
