949843191360d273d972aeb91482b00d327cbf27
voytekio <voytek@voytek.io>
2018-11-24 01:56:51 +0000

    docker...

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,7 +18,11 @@ def somefixture():
 @pytest.fixture
 def docker_cr(request):
     #pdb.set_trace()
-    docker_id = subprocess.check_output(['docker', 'run', '-d', 'alpine', 'sleep', '300']).decode().strip()
+    if on_jenkins:
+        workspace_var = os.environ.get('WORKSPACE','unable')
+        docker_id = subprocess.check_output(['docker', 'run', '-d', '-v', workspace_var+'/logs:/jenkins_logs', 'alpine', 'sleep', '300']).decode().strip()
+    else:
+        docker_id = subprocess.check_output(['docker', 'run', '-d', '-v', '/home/voytek/repos/salt/logs:/logs', 'alpine', 'sleep', '300']).decode().strip()
     res = testinfra.get_host("docker://" + docker_id)
     yield res
     subprocess.check_call(['docker', 'rm', '-f', docker_id])
--- tests/test_docker_crs.py
+++ tests/test_docker_crs.py
@@ -1,6 +1,13 @@
+def _v_print(text_string):
+    print(dir(text_string))
+    text_list = text_string.stdout.split('\n')
+    for one_line in text_list:
+        print(one_line)
+
 def test_with_container(docker_cr):
     #assert docker_cr.check_output('pwd') == '/'
     res1 = docker_cr.run('ls -l /')
-    print(res1)
+    #print(res1)
+    _v_print(res1)
     assert 1==1
 
